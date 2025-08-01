3078e197538e573a7351dc70190f92337938f63b
KrishnaVeni <21p31a0432@acet.ac.in>
2025-08-01 23:37:03 +0530

    ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Routes, Route} from 'react-router';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/orderspage';
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route index element = {<HomePage/>}/>
+      <Route path="/" element={<HomePage/>}></Route>
+      <Route path='checkout' element = {<CheckoutPage/>}></Route>
+      <Route path='orders' element = {<OrdersPage/>}></Route>
+    </Routes>
+    
+  )
+}
+
+export default App
